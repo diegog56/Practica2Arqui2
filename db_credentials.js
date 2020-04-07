adefd530ae0af174512ed4a83186cebb260db3c3
Diego <gomezefdiego@gmail.com>
2020-04-06 19:09:54 -0600

    Initial commit

---
+++ db_credentials.js
@@ -0,0 +1,7 @@
+let db_credentials = {
+    host: "us-cdbr-iron-east-01.cleardb.net",
+    user: "b793b1aa747f2f",
+    password: "fcd8f93c",
+    database: "heroku_e588e6062ea7e96"
+}
+module.exports = db_credentials;
---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json({ limit: '10mb', extended: true }));
+var cors = require('cors');
+app.use(cors());
+
+const db_credentials = require('./db_credentials');
+var conn = mysql.createPool(db_credentials);
+
+
+/*APP*/
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Server running at http://localhost:' + port);
+});
+
+app.get('/', function (req, res) {
+    /*require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+        res.send('Server running at ' + add + ':' + port);
+    })*/
+    res.send('Welcome to Practica2 Arqui2');
+});
+
