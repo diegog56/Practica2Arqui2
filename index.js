98ad85a5e31b09c1e4d87d4ec3800e3f69379ca9
Diego <gomezefdiego@gmail.com>
2020-04-06 20:01:49 -0600

    Sockets communication initialized

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const http = require('http');
 const app = express();
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 var cors = require('cors');
 app.use(cors());
+
+const server = http.createServer(app);
+
+const WebSocket = require('ws');
+const socket = new WebSocket.Server({server});
 
 const db_credentials = require('./db_credentials');
 var conn = mysql.createPool(db_credentials);
@@ -24,3 +30,49 @@ app.get('/', function (req, res) {
     res.send('Welcome to Practica2 Arqui2');
 });
 
+socket.on('connection', function (ws, req) {
+    ws.on('message', function (message) {
+      var json = 0;
+      try{
+      json = JSON.parse(message);
+      }
+      catch(e){
+          json = 0;
+      }
+      if(json === 0){
+        var data = message.split('#');
+        console.log(data);
+        report = new Report({
+          location: {
+            latitude: Number.parseFloat(data[0]),
+            longitude: Number.parseFloat(data[1]),
+            height: Number.parseFloat(data[2])
+          }
+        });
+        report.date = new Date(Date.now() - 6*60*60*1000);
+        console.log(report);
+        report.save((err,rep)=>{
+          if (err) {
+            throw err;
+          }
+          console.log('Reporte guardado');
+        });
+        message = '{"alarma" : true}';
+      }
+      else{
+        if(json.Data)
+          message = 'A';
+        else
+          message = 'B';
+      }
+      socket.clients.forEach(function (client) {
+        if (client != ws && client.readyState) {
+          client.send(message);
+        }
+      });
+    });
+    ws.on('close', function () {
+      console.log("lost one client");
+    });
+    console.log("new client connected");
+  });
